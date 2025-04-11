7e6ef1be2f3f48f533532c2bb44e227301a492fa
Hariom009 <hariomconnects03@gmail.com>
2025-04-12 00:59:56 +0530

    Initial Commit

---
+++ Bread&Butter/Bread_ButterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bread_ButterApp.swift
+//  Bread&Butter
+//
+//  Created by Hari's Mac on 12.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Bread_ButterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bread&ButterTests/Bread_ButterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bread_ButterTests.swift
+//  Bread&ButterTests
+//
+//  Created by Hari's Mac on 12.04.2025.
+//
+
+import Testing
+@testable import Bread_Butter
+
+struct Bread_ButterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
